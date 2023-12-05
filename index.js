3e983f10d53c02a22d5eb4ba20ae9cc3274b843e
Loyd Osborne <loyd.osborne@scyted.tv>
2023-12-05 09:07:53 -0700

    Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch the JSON file
+    fetch('folders.json')
+        .then(response => response.json())
+        .then(data => generateFolders(data.folders))
+        .catch(error => console.error('Error fetching JSON:', error));
+
+    function generateFolders(folders) {
+        const foldersSection = document.getElementById('foldersSection');
+
+        folders.forEach(folder => {
+            const folderElement = document.createElement('section');
+            folderElement.innerHTML = `
+                <h2>${folder.name}</h2>
+                <ul>
+                    <li><a href="${folder.location}">${folder.name}</a></li>
+                </ul>
+            `;
+            foldersSection.appendChild(folderElement);
+        });
+    }
+});
